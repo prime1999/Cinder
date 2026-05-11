26a5db74130c28485fc91e1abb81e6276b032c6f
Prime <primetechnologies02@gmail.com>
2026-05-11 01:16:14 +0100

    made changes to the verify page

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { Suspense } from "react";
 import VerifyContent from "@/components/verify-content";
 
 const Page = () => {
   return (
-    <Suspense fallback={<VerifyContent loadingOnly />}>
+    <Suspense fallback={<div>Loading Verification...</div>}>
       <VerifyContent />
     </Suspense>
   );
